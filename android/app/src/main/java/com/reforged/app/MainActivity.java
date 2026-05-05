e03c96af4ac53593b2909f0f371a064c56c844de
Reforged414 <ferrarifanboi0@gmail.com>
2026-05-05 22:50:43 +0000

    Fix workflow

---
+++ android/app/src/main/java/com/reforged/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.reforged.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.reforged.discipline',
+  appName: 'Reforged',
+  webDir: 'dist'
+};
+
+export default config;

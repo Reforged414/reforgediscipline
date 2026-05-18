46ab127cf6911885306bb1e8ae098b95f90f65e0
Reforged414 <ferrarifanboi0@gmail.com>
2026-05-18 22:03:16 +0000

    fix: move redirect scheme injection to actions workflow using plutil

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,13 +2,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.reforged.discipline',
-  appName: 'reforgediscipline',
+  appName: 'Reforged Discipline',
   webDir: 'dist',
   plugins: {
     GoogleSignIn: {
-      clientId: '901513190581-kebnm9ij83851i249b10mk286gi2r0ov.apps.googleusercontent.com',
-    },
-  },
+      providers: ['google.com'],
+      iosClientId: '901513190581-kebnm9ij83851i249b10mk286gi2r0ov.apps.googleusercontent.com'
+    }
+  }
 };
 
 export default config;
